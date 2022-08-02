94a10a6aa31ba3b6c24bd2ceb0a8d30102cf4fa4
Cosqun <Gulm_be75@itstep.edu.az>
2022-08-02 18:51:19 +0400

    eaw

---
+++ GasOil/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GasOil
+{
+   public  class Oil
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+   
+
+
+}
--- GasOil/Form1.cs
+++ GasOil/Form1.cs
@@ -15,16 +15,49 @@ namespace GasOil
         public Form1()
         {
             InitializeComponent();
+            Oil ai92 = new Oil
+            {
+                Name = "AI-92",
+                Price = 1
+            };
+            Oil ai95 = new Oil
+            {
+                Name = "AI-95",
+                Price = 2
+            };
+            Oil ai98 = new Oil
+            {
+                Name = "AI-98",
+                Price = 2.3
+            };
+            Oil dizel = new Oil
+            {
+                Name = "Dizel",
+                Price = 0.8
+            };
+            List<Oil> oils = new List<Oil>();
+            oils.Add(ai92);
+            oils.Add(ai95);
+            oils.Add(ai98);
+            oils.Add(dizel);
+
         }
 
-        private void label7_Click(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-           
+            
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
-
+            if(comboBox1.SelectedIndex == 0)
+            {
+                label3.Text = "1";
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                
+            }
         }
     }
 }
